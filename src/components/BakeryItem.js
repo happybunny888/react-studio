23d27430a81c9facb80590309eb37d859285373a
happybunny888 <114937017+happybunny888@users.noreply.github.com>
2022-11-09 10:43:58 -0500

    Update BakeryItem.js

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,14 @@
 // TODO: create a component that displays a single bakery item
+
+function BakeryComponent(item){
+    return (
+        <img
+          className={item.name}
+          src={require(item.image)}
+          alt={item.description}
+        />
+        //<p>Bakery Item {item.image}</p>
+        //console.log(item)
+      );
+}
+export default BakeryComponent;
